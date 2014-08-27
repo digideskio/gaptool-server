ab64623baa7c4e31c4a63bfbfc1b8796a6786989
Giacomo Bagnoli <gbagnoli@gmail.com>
2014-08-27 17:34:29 +0100

    Removed remote mongodb feature

--- lib/helpers/init.rb
+++ lib/helpers/init.rb
@@ -11,9 +11,6 @@ GaptoolServer.helpers GaptoolBaseHelpers
 require_relative 'redis'
 GaptoolServer.helpers RedisHelpers
 
-require_relative 'mongodb'
-GaptoolServer.helpers MongodbHelpers
-
 require_relative 'services'
 GaptoolServer.helpers ServiceHelpers
 
--- lib/routes/init.rb
+++ lib/routes/init.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 require_relative 'main'
 require_relative 'redis'
-require_relative 'mongodb'
 require_relative 'services'
 require_relative 'rehash'
