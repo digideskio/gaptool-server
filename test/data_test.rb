2ef0f1d5b7ea02720a62ba77bcb0ccfd3b748955
Giacomo Bagnoli <gbagnoli@gmail.com>
2014-10-05 12:16:21 +0100

    Added tests

--- test/data_test.rb
+++ test/data_test.rb
@@ -10,11 +10,14 @@ describe "Data helper tests" do
     DH.set_config('chefbranch', 'master')
   end
 
-  it "should add a server" do
-    data = {
+  def data
+    {
       'role' => "role",
       'environment' => "env"
     }
+  end
+
+  it "should add a server" do
     expect { DH.addserver("i-1234567", {}, "secret") }.to raise_error(ArgumentError)
     expect { DH.addserver('', data, "secret") }.to raise_error(ArgumentError)
     expect { DH.addserver("i-1234567", data, nil) }.to raise_error(ArgumentError)
@@ -30,4 +33,14 @@ describe "Data helper tests" do
     }))
   end
 
+  it "should register a server" do
+    DH.addserver('i-1234567', data, "secret")
+    res = DH.register_server(data['role'], data['environment'], '')
+    expect(res).to be(nil)
+
+    res = DH.register_server(data['role'], data['environment'], 'secret')
+    expect(res).to eq('i-1234567')
+    server = DH.get_server_data('i-1234567')
+    expect(server['registered']).to be(nil)
+  end
 end
