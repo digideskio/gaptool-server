f89381ccbe70f90bd944bfe6b165a3faee122875
Giacomo Bagnoli <gbagnoli@gmail.com>
2014-10-05 17:01:50 +0100

    Fixed require in tests

--- test/api_test.rb
+++ test/api_test.rb
@@ -1,6 +1,4 @@
-require File.expand_path '../test_helper.rb', __FILE__
-
-include Rack::Test::Methods
+require_relative 'test_helper'
 
 describe "Test API" do
   before(:each) do
@@ -14,4 +12,6 @@ describe "Test API" do
     get '/', {}, {}
     expect(last_response.status).to eq(200)
   end
+
+
 end
