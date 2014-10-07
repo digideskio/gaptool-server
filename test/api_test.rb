d3950d68dba73b6b3abedd83f4f4f8e7d4506526
Giacomo Bagnoli <gbagnoli@gmail.com>
2014-10-07 14:58:31 +0100

    Add terminate tests

--- test/api_test.rb
+++ test/api_test.rb
@@ -24,7 +24,7 @@ describe "Test API" do
       'environment' => 'testenv',
       'ami' => 'ami-1234567',
       'chef_runlist' => 'recipe[myrecipe]',
-      'terminate' => false,
+      'terminate' => true,
       'zone' => 'my-zone-1a',
       'itype' => 'm1.type'
     }
@@ -35,7 +35,7 @@ describe "Test API" do
     expect(last_response.status).to eq(200)
   end
 
-  it "should create a server" do
+  it "should create a instance" do
     post '/init', host_data.to_json
     expect(last_response.status).to eq(200)
     expect(last_response.content_type).to eq('application/json')
@@ -47,11 +47,34 @@ describe "Test API" do
     expect(last_response.status).to eq(400)
   end
 
-  it "should fail to create a server" do
+  it "should fail to create a instance" do
     %w(role environment zone itype).each do |req|
       post '/init', host_data.reject{|k, v| k == req}.to_json
       expect(last_response.status).to eq(400)
       expect(JSON.parse(last_response.body).keys).to eq(%w(result message))
     end
   end
+
+  it "should fail to terminate non-existing instance" do
+    post '/terminate', {'id' => 'i-1234567'}.to_json
+    expect(last_response.status).to eq(404)
+  end
+
+  it "should fail to terminate non-terminable instance" do
+    post '/init', host_data.merge('terminate' => false).to_json
+    expect(last_response.status).to eq(200)
+    id = JSON.parse(last_response.body)['instance']
+    post '/terminate', {'id' => id}.to_json
+    expect(last_response.status).to eq(409)
+  end
+
+  it "should terminate instance" do
+    post '/init', host_data.to_json
+    expect(last_response.status).to eq(200)
+    id = JSON.parse(last_response.body)['instance']
+    post '/terminate', {'id' => id}.to_json
+    expect(last_response.status).to eq(200)
+    expect(JSON.parse(last_response.body)).to eq({id => {'status'=> 'terminated'}})
+  end
+
 end
