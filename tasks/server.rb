df53ceb16686b9d205ba8c74edf86ba2576c569c
Stefano Fontanelli <s.fontanelli@gmail.com>
2014-12-10 11:16:44 -0800

    Add errors handling in server rake task

--- tasks/server.rb
+++ tasks/server.rb
@@ -1,6 +1,11 @@
 namespace :server do
   desc "Set terminable flag to true/false for the instance :instance"
   task :set_terminable, [:instance, :value] do |t, args|
+    if args[:instance].nil? || args[:value].nil?
+      puts "You must specify instance and true/false"
+      return
+    end
     DH.set_server_data_attr args[:instance], 'terminable', (args[:value] == "true" ? "true" : "false")
+    puts DH.get_server_data args[:instance]
   end
 end
