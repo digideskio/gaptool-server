f483a742e4ffe37e5623bf1ad17fa92f7861d1a6
Giacomo Bagnoli <gbagnoli@gmail.com>
2014-12-10 12:38:24 +0000

    Remove rake tasks to manage images

--- tasks/docker.rb
+++ tasks/docker.rb
@@ -10,24 +10,7 @@ unless File.exists?('/.dockerenv')
         printimages
       end
 
-      desc "Build the release image"
-      task :release do
-        sys(%w(./scripts/build_docker_images.sh -t release))
-        printimages
-      end
-
       task :all => [:image]
-    end
-
-    namespace :push do
-      task :release do
-        sys(%w(docker push gild/gaptool:release))
-      end
-
-      desc 'Push all tags to the Docker Hub'
-      task :all do
-        sys(%w(docker push gild/gaptool))
-      end
     end
 
     desc 'Build the docker image'
@@ -41,11 +24,6 @@ unless File.exists?('/.dockerenv')
     desc "Run tests w/ docker"
     task :test => :build do
       sys(%w(fig run --rm gaptool rake test))
-    end
-
-    desc "Rehash instances"
-    task :rehash => :build do
-      sys(%w(fig run --rm gaptool rake rehash))
     end
 
     desc "Stop docker containers"
