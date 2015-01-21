1758f783cc9d497c201cb90fc6b438c524264620
Giacomo Bagnoli <gbagnoli@gmail.com>
2015-01-21 17:07:32 +0000

    Add helpers to rehash a single/multiple property(s) of a single host

--- lib/helpers/rehash.rb
+++ lib/helpers/rehash.rb
@@ -27,12 +27,46 @@ module Gaptool
             "hostname" => instance.public_dns_name,
             "launch_time" => instance.launch_time.to_s,
             "apps" => roles[role].to_s,
-            "instance"=> instance.instance_id
+            "instance"=> instance.instance_id,
+            "security_group" => instance.security_groups[0].name
           }
           Gaptool::Data::addserver(iid, data, nil)
         end
       end
       return {"action" => "complete"}
+    end
+
+    def self.rehash_property(property)
+      Gaptool::Data::servers.each do |id|
+        rehash_property_for_instance(property, id)
+      end
+    end
+
+    def self.rehash_properties_for_instance(instance_id)
+      %w[hostname itype security_group].each do |property|
+        self.rehash_property_for_instance(property, instance_id)
+      end.all?
+    end
+
+    def self.rehash_property_for_instance(property, instance_id)
+      data = Gaptool::Data::get_server_data(instance_id)
+      property = property.to_s
+      return false if data[property].nil? || data[property].empty?
+      Gaptool::EC2::configure_ec2 data['zone']
+      ec2 = AWS::EC2.new
+      instance = ec2.instances[instance_id]
+      case property
+      when "hostname"
+        data[property] = instance.public_dns_name
+      when "itype"
+        data[property] = instance.instance_type
+      when "security_group"
+        data[property] = instance.security_groups[0].name
+      else
+        return false
+      end
+      Gaptool::Data.save_server_data(instance_id, data)
+      true
     end
   end
 end
